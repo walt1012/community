73cc8af5e036659249b974f9bd309c9210d7709f
walt1012 <walt1012@foxmail.com>
2020-02-18 22:08:28 +0800

    extract github config from code to properties

--- src/main/java/com/walt/community/controller/OAuthController.java
+++ src/main/java/com/walt/community/controller/OAuthController.java
@@ -4,6 +4,7 @@ import com.walt.community.dto.AccessTokenDTO;
 import com.walt.community.dto.GithubUser;
 import com.walt.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,14 +22,23 @@ public class OAuthController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String cilentId;
+
+    @Value("${github.cilent.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("callback")
     public String callBack(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id("b99638cde60f721507e9");
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
-        accessTokenDTO.setClient_secret("fb60785efd7895796d6023aec0db20966fedfa01");
+        accessTokenDTO.setClient_id(cilentId);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
