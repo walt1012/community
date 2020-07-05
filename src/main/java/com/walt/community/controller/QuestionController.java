adcdcdfb363e2c738fec8c3361f7c151bc4c125d
walt1012 <walt1012@foxmail.com>
2020-07-05 17:44:29 +0800

    fix login problems

---
+++ src/main/java/com/walt/community/controller/QuestionController.java
@@ -0,0 +1,30 @@
+package com.walt.community.controller;
+
+import com.walt.community.dto.QuestionDTO;
+import com.walt.community.model.User;
+import com.walt.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author: walt1012
+ * @date: 2020/5/20
+ */
+@Controller
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/question/{id}")
+    public String question(@PathVariable(name = "id") Integer id, Model model, HttpServletRequest request) {
+        QuestionDTO questionDTO = questionService.getById(id);
+        model.addAttribute("question", questionDTO);
+        return "question";
+    }
+}
