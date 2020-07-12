ee8849b8a8753841cf1825454e728947bd8d6e1b
walt1012 <walt1012@foxmail.com>
2020-07-12 11:31:02 +0800

    login with no refresh

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -17,7 +17,15 @@ function post() {
             if (response.code == 200) {
                 $("#comment-section").hide();
             } else {
-                alert(response.message)
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=b99638cde60f721507e9&redirect_uri=http://localhost:8887/callback&scope=user&state=1")
+                        window.localStorage.setItem("closable",true)
+                    }
+                } else {
+                    alert(response.message)
+                }
             }
         }
     })
