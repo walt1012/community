c2ddd9e91daa87090791ca269b3d7cff38f45e4a
walt1012 <walt1012@foxmail.com>
2020-08-31 21:56:40 +0800

    fix sql script

--- src/main/resources/db/migration/V8__Change_creator_column_type.sql
+++ src/main/resources/db/migration/V8__Change_creator_column_type.sql
@@ -1,2 +1,2 @@
-alter table question modify creator bigint default not null;
-alter table comment modify commentator bigint default not null;
+alter table question modify creator bigint not null;
+alter table comment modify commentator bigint not null;
